f52cc487afc3436b40b88f71dd7685eb8b9853c1
Jordan Shaw <jordan.shaw@textnow.com>
2023-04-17 23:26:07 -0500

    python lsp
    Signed-off-by: Jordan Shaw <jordan.shaw@textnow.com>

--- nvim/lua/lsp/init.lua
+++ nvim/lua/lsp/init.lua
@@ -98,3 +98,20 @@ nvim_lsp.sumneko_lua.setup {
     },
 }
 
+nvim_lsp.pyright.setup{
+    cmd = { "pyright-langserver", "--stdio" },
+    filetypes = { "python" },
+    --root_dir = function(startpath)
+    --       return M.search_ancestors(startpath, matcher)
+    --  end,
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          diagnosticMode = "workspace",
+          useLibraryCodeForTypes = true
+        },
+      },
+    },
+    single_file_support = true
+}
